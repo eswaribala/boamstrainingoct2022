b950698db2d894ea58c7701500eb3662d80bb29b
eswaribala <parameswaribala@gmail.com>
2022-10-10 13:00:46 +0530

    customer project modified

---
+++ customerapi/customerapi/src/main/java/com/boa/customerapi/models/Corporate.java
@@ -0,0 +1,17 @@
+package com.boa.customerapi.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+import lombok.experimental.SuperBuilder;
+@SuperBuilder
+@Entity
+@Table(name="Corporate")
+public class Corporate extends Customer{
+    @Enumerated(EnumType.STRING)
+    @Column(name="Company_Type",nullable = false)
+	private CompanyType companyType;
+
+}
