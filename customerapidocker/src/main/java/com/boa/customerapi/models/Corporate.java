232573e18b45692f4acfa0472657a04fcbfec98f
eswaribala <parameswaribala@gmail.com>
2022-10-12 15:21:28 +0530

    updated zuul

---
+++ customerapidocker/src/main/java/com/boa/customerapi/models/Corporate.java
@@ -0,0 +1,26 @@
+package com.boa.customerapi.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+@SuperBuilder
+@Entity
+@Table(name="Corporate")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class Corporate extends Customer{
+    @Enumerated(EnumType.STRING)
+    @Column(name="Company_Type",nullable = false)
+	private CompanyType companyType;
+
+}
